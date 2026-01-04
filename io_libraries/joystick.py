840925f92905c0f5684975ecad7e68d9f7bf3388
Nicholas George <nicokofi@gmail.com>
2026-01-04 12:26:41 -0500

    added libraries for reading from ADC

---
+++ io_libraries/joystick.py
@@ -0,0 +1,19 @@
+import os
+import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D22)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 0
+chan0 = AnalogIn(mcp, MCP.P0)
